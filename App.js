f1886143e76eed6228b8f59374afce20c94976b9
Louis <louis.hsiao@d8ai.com>
2018-06-27 11:08:54 +0800

    rm shit

--- App.js
+++ App.js
@@ -10,19 +10,10 @@ import {
   Text,
   TouchableOpacity,
   View,
-  Platform,
 } from 'react-native';
 import { RNCamera } from 'react-native-camera';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-type Props = {};
-export default class App extends Component<Props> {
+export default class App extends Component {
   componentDidMount(){
     setTimeout(this.takePicture,1000);
   }
